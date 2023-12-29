174b0bb5b192c216034cf7bf0dccc2961b513608
Caio Alves <caioalvesfig@gmail.com>
2023-12-29 12:29:22 -0300

    fix: refactor tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,23 +5,7 @@ RSpec.describe Game, type: :model do
     invalid_statuses = ['waiting for opponent', 'Some other status', nil]
 
     invalid_statuses.each do |status|
-      game = Game.new(
-        token_1: SecureRandom.hex(10),
-        token_2: SecureRandom.hex(10),
-        board_state: [
-          [-1, 0, -1, 0, -1, 0, -1, 0],
-          [0, -1, 0, -1, 0, -1, 0, -1],
-          [-1, 0, -1, 0, -1, 0, -1, 0],
-          [0, 0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0, 0],
-          [0, 1, 0, 1, 0, 1, 0, 1],
-          [1, 0, 1, 0, 1, 0, 1, 0],
-          [0, 1, 0, 1, 0, 1, 0, 1]
-        ],
-        player_1_pieces: 12,
-        player_2_pieces: 12,
-        game_status: status
-      )
+      game = build(:game, game_status: status)
 
       expect(game).to_not be_valid
     end
@@ -31,23 +15,7 @@ RSpec.describe Game, type: :model do
     game_status_options = ['Waiting for opponent', 'Player_1 turn', 'Player_2 turn', 'Player_1 won', 'Player_2 won']
 
     game_status_options.each do |status|
-      game = Game.new(
-        token_1: SecureRandom.hex(10),
-        token_2: SecureRandom.hex(10),
-        board_state: [
-          [-1, 0, -1, 0, -1, 0, -1, 0],
-          [0, -1, 0, -1, 0, -1, 0, -1],
-          [-1, 0, -1, 0, -1, 0, -1, 0],
-          [0, 0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0, 0],
-          [0, 1, 0, 1, 0, 1, 0, 1],
-          [1, 0, 1, 0, 1, 0, 1, 0],
-          [0, 1, 0, 1, 0, 1, 0, 1]
-        ],
-        player_1_pieces: 12,
-        player_2_pieces: 12,
-        game_status: status
-      )
+      game = build(:game, game_status: status)
 
       expect(game).to be_valid
       expect(game.save).to eq(true)
